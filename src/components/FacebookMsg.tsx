99b45ad7a6ff49b06031879be4d7f7431f8344b1
Dileep Verma <dileepverma107@gmail.com>
2024-07-09 12:11:54 +0530

    Update FacebookMsg.tsx

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -2,7 +2,7 @@
 import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookMsg = () => {
 return (
-<FacebookProvider appId="448839711330615" chatSupport>
+<FacebookProvider appId="443629761883877" chatSupport>
 <CustomChat pageId="301636533825900" minimized={true} />
 </FacebookProvider>
 );
