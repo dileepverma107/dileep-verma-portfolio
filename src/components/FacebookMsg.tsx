ec6df511466455418816db0ac4be657238b5d5e7
Dileep Verma <dileepverma107@gmail.com>
2024-07-09 16:59:17 +0530

    commit

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -2,7 +2,7 @@
 import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookMsg = () => {
 return (
-<FacebookProvider appId="443629761883877" chatSupport>
+<FacebookProvider appId="1172989773748447" chatSupport>
 <CustomChat pageId="301636533825900" minimized={true} />
 </FacebookProvider>
 );
