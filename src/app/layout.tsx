74f05e91e738841a8ce2c2e9243e9fff86b7f4dd
Dileep Verma <dileepverma107@gmail.com>
2024-07-09 16:49:33 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import FacebookMsg from "@/components/FacebookMsg";
+import FacebookMessenger from "@/components/FacebookMessenger";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -64,7 +64,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
-          <FacebookMsg/>
+          <FacebookMessenger/>
             {children}
             <Navbar />
           </TooltipProvider>
